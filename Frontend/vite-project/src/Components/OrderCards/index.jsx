0ddcf601b9e8505ec35d4d345c6d8cfea98c2830
Rayito391 <ryangpe153@gmail.com>
2024-07-04 23:59:25 -0700

    CasiAcabo

--- Frontend/vite-project/src/Components/OrderCards/index.jsx
+++ Frontend/vite-project/src/Components/OrderCards/index.jsx
@@ -1,7 +1,7 @@
 const OrderCard = props => {
-    const {id, title, imageURL, price} = props
+    const {title, imageURL, price} = props
     return(
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center mb-3'>
             <div className='flex items-center gap-2'>
                 <figure className='w-20 h-20'>
                     <img className='w-full h-full rounded-lg object-cover' src={imageURL} alt={title} />
@@ -12,11 +12,13 @@ const OrderCard = props => {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>
-                    {price}
+                    ${price}
                 </p>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
+
+
             </div>
         </div>
     )
