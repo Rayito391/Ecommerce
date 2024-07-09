0d6a2b1d0ae4f6e9465b362c475912aa14f013b8
Rayito391 <ryangpe153@gmail.com>
2024-07-08 17:58:20 -0700

    Filtrado de productos en search

--- Frontend/vite-project/src/Pages/Home/index.jsx
+++ Frontend/vite-project/src/Pages/Home/index.jsx
@@ -1,27 +1,56 @@
-import { useState,useEffect } from "react";
+import { useContext } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 import ProductDetail from "../../Components/ProductDetail";
+import { ShoppingContext } from "../../Context";
 
 function Home() {
-    const [items, setItems] = useState(null)
-
-    useEffect(() => {
-      fetch('https://api.escuelajs.co/api/v1/products')
-      .then(response => response.json())
-      .then(data => setItems(data))
-    },[])
-    return (
-        <Layout>
-          <div className="mt-6 grid gap-4 grid-cols-3 w-full max-w-screen-xl">
-            {
-              items?.map(item => (
+    const context = useContext(ShoppingContext)
+    const renderView = () => {
+      if(context.searchByTitle?.length > 0){
+          if(context.filteredItems?.length > 0){
+            return (
+              context.filteredItems?.map(item => (
                 <Card
                   key={item.id}
                   data={item}
                 />
               ))
-            }
+            )
+          }else{
+            return (
+              <div className='flex font-bold text-3xl'> We don't have anything :C</div>
+            )
+          }
+      }else{
+        return(
+          context.items?.map(item => (
+            <Card
+              key={item.id}
+              data={item}
+            />
+          ))
+        )
+      }
+    }
+
+
+    return (
+        <Layout>
+          <div className='flex justify-between'>
+            <div className='bg-white rounded-full top-0'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mt-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </div>
+            <input 
+            type="text" 
+            placeholder="Search a Product" 
+            className='ml-2 rounded-lg border p-4 mb-4 border-black w-96 focus:outline-none'
+            onChange={(event)=> context.setSearchByTitle(event.target.value)}/>
+          </div>
+          <div className="mt-6 grid gap-4 grid-cols-3 w-full max-w-screen-xl">
+            {renderView()}
           </div>
           <ProductDetail/>
         </Layout>
--- Frontend/vite-project/src/Pages/MyOrder/index.jsx
+++ Frontend/vite-project/src/Pages/MyOrder/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from "react-router-dom";
 
 import Layout from "../../Components/Layout";
 import { ShoppingContext } from "../../Context";
@@ -6,13 +7,24 @@ import OrderCard from '../../Components/OrderCard';
 
 function MyOrder() {
     const context = useContext(ShoppingContext)
+    const currentPath = window.location.pathname
+    let indexPath = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+    if (indexPath === 'last') indexPath = context.order?.length-1 
+
     return (
         <Layout>
-          MyOrder
+            <div className='mt-6 mb-6 flex relative justify-center w-80 items-center'>
+                <Link to='/myorders'  className='absolute left-0'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                </svg>
+                </Link>
+                <h1 className='font-medium text-xl mb-4'>My Order</h1>
+            </div>
+          
           <div className='flex flex-col w-80'>
                 {
-                    context.order && context.order.length > 0 ?
-                    context.order?.slice(-1)[0].products.map(product => (
+                    context.order?.[indexPath]?.products.map(product => (
                         <OrderCard
                             key={product.id}
                             id = {product.id}
@@ -20,7 +32,7 @@ function MyOrder() {
                             imageURL={product.images}
                             price={product.price}
                         />
-                    ))  : <p>No hay productos en la orden.</p>
+                    ))
                 }
             </div>
         </Layout>
